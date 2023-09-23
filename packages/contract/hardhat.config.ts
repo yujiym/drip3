60f6cdbe8a038f6d45613190caeecfb48eb505e5
Yuji Yamaguchi <yuji.ym@gmail.com>
2023-09-23 15:47:19 -0400

    contract update

--- packages/contract/hardhat.config.ts
+++ packages/contract/hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
+  },
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY || ""],
+    }
+  }
 };
 
 export default config;
--- packages/contract/scripts/deploy.ts
+++ packages/contract/scripts/deploy.ts
@@ -1,22 +1,13 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [deployer] = await ethers.getSigners();
 
-  const lockedAmount = ethers.parseEther("0.001");
+  console.log("Deploying contracts with the account:", deployer.address);
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const token = await ethers.deployContract("Drip3NFT");
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("Token address:", await token.getAddress());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
