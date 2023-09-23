e19eae3c13a4400455137c993916db38a99d8b89
Yuji Yamaguchi <yuji.ym@gmail.com>
2023-09-23 06:56:48 -0400

    setup web3modal, web3inbox, vercel blob, vercel postgres

--- packages/frontend/app/page.tsx
+++ packages/frontend/app/page.tsx
@@ -1,15 +1,23 @@
+"use client";
 import Image from "next/image";
+import { useEffect } from "react";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+import { useAccount } from "wagmi";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const { open } = useWeb3Modal();
+  const { address, isConnecting, isDisconnected } = useAccount();
+
+  useEffect(() => {
+    if (!isDisconnected && address) {
+      redirect("/app");
+    }
+  }, [address, isConnecting, isDisconnected]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"></div>
-      </div>
+    <main>
+      <button onClick={() => open()}>Start Selling</button>
     </main>
   );
 }
--- packages/frontend/app/providers.tsx
+++ packages/frontend/app/providers.tsx
@@ -1,10 +1,26 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
+import { WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID ?? "";
+const chains = [mainnet, arbitrum, polygon];
+const wagmiConfig = defaultWagmiConfig({
+  chains,
+  projectId,
+  appName: "drip3",
+});
+createWeb3Modal({ wagmiConfig, projectId, chains });
 
 export default function Providers({
   children,
 }: {
   children: React.ReactNode;
 }): React.ReactNode {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>
+    </NextUIProvider>
+  );
 }
