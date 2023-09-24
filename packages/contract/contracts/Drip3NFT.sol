560dc4ab0ffaff27fe0b71dbbf3eeacb882a37ff
Yuji Yamaguchi <yuji.ym@gmail.com>
2023-09-23 23:51:02 -0400

    update nft

--- packages/contract/contracts/Drip3NFT.sol
+++ packages/contract/contracts/Drip3NFT.sol
@@ -11,6 +11,7 @@ contract Drip3NFT is ERC721URIStorage {
     Counters.Counter private _tokenIds;
     Counters.Counter private _itemsSold;
 
+    mapping(bytes32 => uint256) public idToCid;
     uint256 listingPrice = 0 ether;
 
     address payable owner;
@@ -50,7 +51,7 @@ contract Drip3NFT is ERC721URIStorage {
     }
 
     function createToken(
-        string memory tokenURI,
+        bytes32 cid,
         uint256 price
     ) public payable returns (uint) {
         _tokenIds.increment();
@@ -58,10 +59,16 @@ contract Drip3NFT is ERC721URIStorage {
         uint256 newTokenId = _tokenIds.current();
 
         _mint(msg.sender, newTokenId);
-        _setTokenURI(newTokenId, tokenURI);
+        // set fileId
+        _setBytesForId(cid, newTokenId);
+
         createMarketItem(newTokenId, price);
 
         return newTokenId;
+    }
+
+    function _setBytesForId(bytes32 fileId, uint256 tokenId) internal {
+        idToCid[fileId] = tokenId;
     }
 
     function createMarketItem(uint256 tokenId, uint256 price) private {
